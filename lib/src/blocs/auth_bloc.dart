195c6c1d3beb442c407cd36772eba0062ddfff5d
Rukshana <nagarkotirukshanaa@gmail.com>
2021-10-02 23:13:37 +0545

    completed scoped_bloc

--- lib/src/blocs/auth_bloc.dart
+++ lib/src/blocs/auth_bloc.dart
@@ -1,12 +1,13 @@
 import 'dart:async';
 
 import 'package:login_bloc/src/mixins/auth_validation_mixing.dart';
+import 'package:rxdart/rxdart.dart';
 
 class AuthBloc with AuthValidationMixin {
 
-  final StreamController<String> _emailController = StreamController();
-  final StreamController<String>  _passwordController = StreamController();
-  final StreamController<String>  _genderController = StreamController();
+  final BehaviorSubject<String> _emailController = BehaviorSubject();
+  final BehaviorSubject<String>  _passwordController =BehaviorSubject();
+  final BehaviorSubject<String>  _genderController = BehaviorSubject();
 
   //getters ro add values to stream
   Function(String val) get addEmail => _emailController.sink.add;
@@ -21,7 +22,17 @@ class AuthBloc with AuthValidationMixin {
   Stream<String> get genderStream =>_genderController
       .stream.transform(genderValidator);
 
-void dispose(){
+  Stream<bool> get submitStream => CombineLatestStream.combine3(
+    emailStream, passwordStream, genderStream, (a,b,c) => true);
+
+  void submitData(){
+    String email = _emailController.value;
+    String password = _passwordController.value;
+    String gender = _genderController.value;
+  print("Email is $email, password is $password and gender is $gender");
+  }
+
+  void dispose(){
   _emailController.close();
   _passwordController.close();
   _genderController.close();
--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -3,12 +3,13 @@ import 'package:login_bloc/src/blocs/auth_bloc.dart';
 import 'package:login_bloc/src/blocs/auth_bloc_provider.dart';
 
 class LoginScreen extends StatelessWidget {
-   LoginScreen({Key? key}) : super(key: key);
- // final bloc = AuthBloc();
+  LoginScreen({Key? key}) : super(key: key);
+
+  // final bloc = AuthBloc();
 
   @override
   Widget build(BuildContext context) {
-   AuthBloc bloc = AuthBLocProvider.of(context);
+    AuthBloc bloc = AuthBLocProvider.of(context);
     return Container(
       margin: EdgeInsets.all(16),
       child: Column(
@@ -19,7 +20,7 @@ class LoginScreen extends StatelessWidget {
           SizedBox(height: 16),
           buildGenderDropdown(bloc),
           SizedBox(height: 16),
-          buildSubmitButton(),
+          buildSubmitButton(bloc),
 
         ],
       ),
@@ -35,76 +36,83 @@ class LoginScreen extends StatelessWidget {
           decoration: InputDecoration(
             labelText: "Email address",
             hintText: "you@example.com",
-            errorText: snapshot.error?.toString(),
-            // errorText:snapshot.hasError? snapshot.error.toString(): null),
             border: OutlineInputBorder(),
-          ),
-          onChanged: (val) {
-            bloc.addEmail(val);
-          },
-
-        );
+            // errorText: snapshot.error?.toString(),
+            errorText:snapshot.hasError? snapshot.error.toString(): null),
+           onChanged: bloc.addEmail,
+          );
+        //   onChanged: (val) {
+        //     bloc.addEmail(val);
+        //   },
+        //
+        // );
       },
     );
   }
-  Widget buildPasswordField(AuthBloc bloc){
-    return StreamBuilder(
-      stream: bloc.passwordStream,
-      builder: (context, AsyncSnapshot<String> snapshot) {
-        return TextField(
-          obscureText:true,
-          decoration: InputDecoration(
-              labelText: "Your password",
-              hintText: "password",
-              border: OutlineInputBorder(),
-            errorText: snapshot.error?.toString(),
-          ),
-            onChanged: (val) {
-              bloc.addPassword(val);
-            }
-        );
-      }
-    );
 
-    }
+  Widget buildPasswordField(AuthBloc bloc) {
+    return StreamBuilder(
+        stream: bloc.passwordStream,
+        builder: (context, AsyncSnapshot<String> snapshot) {
+          return TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Your password",
+                hintText: "password",
+                border: OutlineInputBorder(),
+                errorText: snapshot.error?.toString(),
+              ),
+              onChanged: bloc.addPassword,
+
+          );
+        }
+    );
+  }
 
   Widget buildGenderDropdown(AuthBloc bloc) {
     return StreamBuilder(
-      stream: bloc.genderStream,
-      builder: (context, AsyncSnapshot<String> snapshot) {
-        return DropdownButtonFormField<String>(
-          items: [
-            DropdownMenuItem(child: Text("Female") ,value: "female"),
-            DropdownMenuItem(child: Text("Male"), value: "male"),
-            DropdownMenuItem(child: Text("I don't wish to answer"), value: "I don't wish to answer"),
-          ],
-          onChanged: (val){
+        stream: bloc.genderStream,
+        builder: (context, AsyncSnapshot<String> snapshot) {
+          return DropdownButtonFormField<String>(
+            items: [
+              DropdownMenuItem(child: Text("Female"), value: "female"),
+              DropdownMenuItem(child: Text("Male"), value: "male"),
+              DropdownMenuItem(child: Text("I don't wish to answer"),
+                  value: "I don't wish to answer"),
+            ],
+            onChanged: (val) {
               bloc.addGender(val!);
-          },
-          decoration: InputDecoration(
+            },
+            decoration: InputDecoration(
               labelText: "Your gender",
               hintText: "male/female",
               border: OutlineInputBorder(),
-          errorText: snapshot.error?.toString(),
-          ),
-        );
-      }
+              errorText: snapshot.error?.toString(),
+            ),
+          );
+        }
     );
   }
 
-  Widget buildSubmitButton(){
+  Widget buildSubmitButton(AuthBloc bloc) {
     return Container(
-      width:double.infinity,
-      child: ElevatedButton(
-        child: Text("Submit"),
-        onPressed: (){},
-        style: ElevatedButton.styleFrom(
-          padding: EdgeInsets.all(12),
-          primary: Colors.teal
-        ),
+      width: double.infinity,
+      child: StreamBuilder(
+          stream: bloc.submitStream,
+          builder: (context, snapshot) {
+            return ElevatedButton(
+              child: Text("Submit"),
+              onPressed: snapshot.hasData ?
+                bloc.submitData :null,
+
+              style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(12),
+                  primary: Colors.teal),
+            );
+          }
       ),
     );
   }
-
-
 }
+
+
